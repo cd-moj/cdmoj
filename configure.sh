6df1f9e622e04b5869b49962a80177e039c1d423
Bruno Ribas <brunoribas@gmail.com>
2013-07-08 23:43:34 -0300

    configure.sh: Really fix sed

--- configure.sh
+++ configure.sh
@@ -6,5 +6,5 @@ if [[ "x$PREFIX" == "x" ]]; then
     exit 0
 fi
 
-sed -ibkp -e "s/#CONFDIR#/$PREFIX\/etc/g" -e "s/#SCRIPTSDIR#/$PREFIX\/scripts/g" \
+sed -ibkp -e "s;#CONFDIR#;$PREFIX/etc;g" -e "s;#SCRIPTSDIR#;$PREFIX/scripts;g" \
     judge/*sh html/cgi-bin/*sh
