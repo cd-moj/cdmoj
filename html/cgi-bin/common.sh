385de16e5f5a8c85b0a6779e9d216c2f48313792
Bruno Ribas <brunoribas@utfpr.edu.br>
2013-08-06 19:55:09 -0300

    common.sh: New indent format
    Signed-off-by: Bruno Ribas <brunoribas@utfpr.edu.br>

--- html/cgi-bin/common.sh
+++ html/cgi-bin/common.sh
@@ -3,37 +3,37 @@ source #CONFDIR#/common.conf
 
 function link-prob-spoj()
 {
-    local SITE=$1
-    local PROBID=$2
-    echo "http://$SITE.spoj.com/problems/$2"
+  local SITE=$1
+  local PROBID=$2
+  echo "http://$SITE.spoj.com/problems/$2"
 }
 function link-prob-spoj-br()
 {
-    link-prob-spoj br $1
+  link-prob-spoj br $1
 }
 
 function link-prob-spoj-www()
 {
-    link-prob-spoj www $1
+  link-prob-spoj www $1
 }
 
 function link-prob-uri()
 {
-    local PROBID=$1
-    echo "http://www.urionlinejudge.com.br/judge/problems/view/$PROBID"
+  local PROBID=$1
+  echo "http://www.urionlinejudge.com.br/judge/problems/view/$PROBID"
 }
 
 function cabecalho-html()
 {
-    printf "Content-type: text/html\n\n"
-    cat ../header.html
-    cat ../menu.html
-    cat << EOF
-                    <div id="text">
+  printf "Content-type: text/html\n\n"
+  cat ../header.html
+  cat ../menu.html
+  cat << EOF
+          <div id="text">
 EOF
 
-    return
-    cat << EOF
+  return
+  cat << EOF
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <HTML>
 <HEAD>
@@ -46,64 +46,64 @@ EOF
 
 function pega-login()
 {
-    local  LOGIN="$(echo "$HTTP_COOKIE"|sed -e 's/COOKIE=/ /' | tr ';' '\n'|grep login=|cut -d'=' -f2)"
-    local  LOGIN=${LOGIN// }
-    echo "$LOGIN"
+  local  LOGIN="$(echo "$HTTP_COOKIE"|sed -e 's/COOKIE=/ /' | tr ';' '\n'|grep login=|cut -d'=' -f2)"
+  local  LOGIN=${LOGIN// }
+  echo "$LOGIN"
 }
 
 function pega-nome()
 {
-    local CONTEST=$1
-    local LOGIN="$(pega-login)"
-    grep "^$LOGIN:" $CONTESTSDIR/$CONTEST/passwd |cut -d':' -f3
+  local CONTEST=$1
+  local LOGIN="$(pega-login)"
+  grep "^$LOGIN:" $CONTESTSDIR/$CONTEST/passwd |cut -d':' -f3
 }
 
 function verifica-login()
 {
-    local SITE="$1"
+  local SITE="$1"
 
-    #verificar COOKIES
+  #verificar COOKIES
 
-    local LOGIN="$(pega-login)"
-    local HASH="$(echo "$HTTP_COOKIE" |sed -e 's/COOKIE=/ /'| tr ';' '\n'|grep hash=|cut -d'=' -f2)"
-    HASH=${HASH// }
+  local LOGIN="$(pega-login)"
+  local HASH="$(echo "$HTTP_COOKIE" |sed -e 's/COOKIE=/ /'| tr ';' '\n'|grep hash=|cut -d'=' -f2)"
+  HASH=${HASH// }
 
 
-    if [[ "x$LOGIN" == "x" ]] ;then
-        echo Nao
-    elif [[ ! -e "$CACHEDIR/$LOGIN-$SITE" ]]; then
-        echo Nao
+  if [[ "x$LOGIN" == "x" ]] ;then
+    echo Nao
+  elif [[ ! -e "$CACHEDIR/$LOGIN-$SITE" ]]; then
+    echo Nao
+  else
+    HASHARMAZENADA="$(< $CACHEDIR/$LOGIN-$SITE)"
+    if [[ "$HASH" != "$HASHARMAZENADA" ]]; then
+      echo Nao
     else
-        HASHARMAZENADA="$(< $CACHEDIR/$LOGIN-$SITE)"
-        if [[ "$HASH" != "$HASHARMAZENADA" ]]; then
-            echo Nao
-        else
-            echo Sim
-        fi
+      echo Sim
     fi
+  fi
 
 }
 
 function tela-erro()
 {
-    cabecalho-html
-    echo "<h1>Ocorreu algum Erro</h1>"
-    cat ../footer.html
+  cabecalho-html
+  echo "<h1>Ocorreu algum Erro</h1>"
+  cat ../footer.html
 }
 
 function tela-login()
 {
-    cabecalho-html
-    printf "<h1>Login em $1</h1>\n"
-    cat << EOF
+  cabecalho-html
+  printf "<h1>Login em $1</h1>\n"
+  cat << EOF
 <form enctype="multipart/form-data" action="$BASEURL/cgi-bin/login.sh/$1" method="post">
-    Login: <input name="login" type="text"><br/>
-    Senha: <input name="senha" type="password"><br/>
-    <br/>
-    <input type="submit" value="Login">
-    <br/>
+  Login: <input name="login" type="text"><br/>
+  Senha: <input name="senha" type="password"><br/>
+  <br/>
+  <input type="submit" value="Login">
+  <br/>
 </form>
 EOF
-    cat ../footer.html
-    exit 0;
+  cat ../footer.html
+  exit 0;
 }
