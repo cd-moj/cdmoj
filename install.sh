d6ea33d02ab829bcaa4a873923e8c90bf3a1d1d0
Bruno Ribas <brunoribas@utfpr.edu.br>
2013-08-06 19:55:41 -0300

    install.sh: New indent format
    Signed-off-by: Bruno Ribas <brunoribas@utfpr.edu.br>

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ PREFIX="$1"
 HTMLDIR="$2"
 
 if (( $# != 2 )); then
-    echo "$0 PREFIX HTMLDIR"
-    exit 0
+  echo "$0 PREFIX HTMLDIR"
+  exit 0
 fi
 
 bash configure.sh "$PREFIX" "$HTMLDIR"
@@ -18,20 +18,20 @@ tar cfj "$HTMLDIR/contests/sample.tar.bz2" sample
 rm -rf sample
 
 if [[ ! -d "$PREFIX/etc/" ]]; then
-    mkdir -p "$PREFIX/etc"
-    echo "[ Ok ] Creating $PREFIX/etc"
+  mkdir -p "$PREFIX/etc"
+  echo "[ Ok ] Creating $PREFIX/etc"
 fi
 
 if [[ ! -e "$PREFIX/etc/common.conf" ]]; then
-    cp etc/common.conf "$PREFIX/etc/common.conf"
-    echo "[ Ok ] Copying common.conf to $PREFIX/etc"
+  cp etc/common.conf "$PREFIX/etc/common.conf"
+  echo "[ Ok ] Copying common.conf to $PREFIX/etc"
 fi
 
 if [[ ! -e "$PREFIX/etc/judge.conf" ]]; then
-    cp etc/judge.conf "$PREFIX/etc/judge.conf"
-    echo "[ Ok ] Copying judge.conf to $PREFIX/etc"
-    chmod 600 "$PREFIX/etc/judge.conf"
-    echo "[ Ok ] chmod 600 $PREFIX/etc/judge.conf"
+  cp etc/judge.conf "$PREFIX/etc/judge.conf"
+  echo "[ Ok ] Copying judge.conf to $PREFIX/etc"
+  chmod 600 "$PREFIX/etc/judge.conf"
+  echo "[ Ok ] chmod 600 $PREFIX/etc/judge.conf"
 fi
 
 echo "============================================="
