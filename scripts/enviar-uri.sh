e2d1a2a4d530f9e802c170e735f0c6a2bd4f7967
Bruno Ribas <brunoribas@utfpr.edu.br>
2013-08-06 19:56:29 -0300

    enviar-uri.sh: New indent format
    Signed-off-by: Bruno Ribas <brunoribas@utfpr.edu.br>

--- scripts/enviar-uri.sh
+++ scripts/enviar-uri.sh
@@ -6,55 +6,55 @@ URILANGS=(C Cpp Java)
 
 function link-prob-uri()
 {
-    PROBID=$1
-    echo "http://www.urionlinejudge.com.br/judge/problems/view/$PROBID"
+  PROBID=$1
+  echo "http://www.urionlinejudge.com.br/judge/problems/view/$PROBID"
 }
 
 function login-uri()
 {
-    #source config
-    curl -s  -c $HOME/.cache/cookie -F "data[User][email]=$USUARIO" \
-               -A "Mozilla/4.0" \
-               -F "data[User][password]=$SENHA" \
-                http://www.urionlinejudge.com.br/judge/pt/users/login \
-                    >/dev/null
+  #source config
+  curl -s  -c $HOME/.cache/cookie -F "data[User][email]=$USUARIO" \
+         -A "Mozilla/4.0" \
+         -F "data[User][password]=$SENHA" \
+        http://www.urionlinejudge.com.br/judge/pt/users/login \
+          >/dev/null
 }
 
 #retorna o ID da submissao
 function enviar-uri()
 {
-    ARQFONTE=$1
-    PROBID=$2
-    LINGUAGEM=$3
+  ARQFONTE=$1
+  PROBID=$2
+  LINGUAGEM=$3
 
-    if [[ "$LINGUAGEM" == "Java" ]];then
-        LINGUAGEM=3;
-    else
-        LINGUAGEM=2;
-    fi
+  if [[ "$LINGUAGEM" == "Java" ]];then
+    LINGUAGEM=3;
+  else
+    LINGUAGEM=2;
+  fi
 
-    #enviar
-    curl -A "Mozilla/4.0" -b ~/.cache/cookie \
-        -F "data[Run][source]=$(<$ARQFONTE)" \
-        -F "data[Run][lang_id]=$LINGUAGEM" \
-        -F "data[Run][problem_id]=$PROBID" \
-        www.urionlinejudge.com.br/judge/runs/add
+  #enviar
+  curl -A "Mozilla/4.0" -b ~/.cache/cookie \
+    -F "data[Run][source]=$(<$ARQFONTE)" \
+    -F "data[Run][lang_id]=$LINGUAGEM" \
+    -F "data[Run][problem_id]=$PROBID" \
+    www.urionlinejudge.com.br/judge/runs/add
 
-    #pegar Codigo da submissao
-    curl -A "Mozilla/4.0" -b ~/.cache/cookie -s\
-        http://www.urionlinejudge.com.br/judge/runs|
-        grep "/judge/runs/code/"|head -n1|cut -d'"' -f2|
-            awk -F'/' '{print $NF}'
+  #pegar Codigo da submissao
+  curl -A "Mozilla/4.0" -b ~/.cache/cookie -s\
+    http://www.urionlinejudge.com.br/judge/runs|
+    grep "/judge/runs/code/"|head -n1|cut -d'"' -f2|
+      awk -F'/' '{print $NF}'
 }
 
 #Retorna string do resultado
 function pega-resultado-uri()
 {
-    JOBID=$1
+  JOBID=$1
+  RESP="$(curl -s -A "Mozilla/4.0" -b ~/.cache/cookie www.urionlinejudge.com.br/judge/runs/code/$JOBID | elinks -dump|grep -A1 Resposta:|tail -n1|sed -e 's/  //g'|sed -e 's/^ //')"
+  while [[ "$RESP" =~ "queue" ]]; do
+    sleep 5
     RESP="$(curl -s -A "Mozilla/4.0" -b ~/.cache/cookie www.urionlinejudge.com.br/judge/runs/code/$JOBID | elinks -dump|grep -A1 Resposta:|tail -n1|sed -e 's/  //g'|sed -e 's/^ //')"
-    while [[ "$RESP" =~ "queue" ]]; do
-        sleep 5
-        RESP="$(curl -s -A "Mozilla/4.0" -b ~/.cache/cookie www.urionlinejudge.com.br/judge/runs/code/$JOBID | elinks -dump|grep -A1 Resposta:|tail -n1|sed -e 's/  //g'|sed -e 's/^ //')"
-    done
-    echo "$RESP"
+  done
+  echo "$RESP"
 }
