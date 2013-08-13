be46550d2aa2d4baf4f405155995481f53d12850
Bruno Ribas <brunoribas@gmail.com>
2013-08-13 18:32:49 -0300

    julgador.sh: Separation between Corrector and everything else

--- judge/julgador.sh
+++ judge/julgador.sh
@@ -5,12 +5,69 @@ source #CONFDIR#/common.conf
 source #SCRIPTSDIR#/enviar-spoj.sh
 source #SCRIPTSDIR#/enviar-uri.sh
 
+function updatescore()
+{
+    contest=$1
+
+    CLASSIFICACAO=1
+    cat $CONTESTSDIR/$contest/controle/*.score|sort -n -t ':' -k3|
+      sort -s -n -r -t ':' -k2|
+      cut -d: -f1|
+      while read LINE; do
+        echo "<tr><td>$CLASSIFICACAO</td>$LINE";
+        ((CLASSIFICACAO++))
+      done  > $CONTESTSDIR/$CONTEST/controle/SCORE
+}
+
+function updatedotscore()
+{
+  local NOME="$2"
+  local LOGIN="$1"
+  local CONTEST="$3"
+
+  source $CONTESTSDIR/$CONTEST/conf
+
+  #gerar arquivo para montar o score
+  local ACUMPENALIDADES=0
+  local ACUMACERTOS=0
+  local TAMARRAY=${#PROBS[@]}
+  #Ordem tabela de score
+  #nome | A | B | C | D | ... | Acertos | Penalidade |
+  {
+    printf "<td>$NOME</td>"
+    for((prob=0;prob<TAMARRAY;prob+=5)); do
+      PENALIDADES=0
+      JAACERTOU=0
+      TENTATIVAS=0
+      PENDING=0
+      source $CONTESTSDIR/$CONTEST/controle/$LOGIN.d/$prob 2>/dev/null
+      if (( TENTATIVAS == 0 )) ; then
+        printf "<td></td>"
+      elif (( JAACERTOU > 0 )); then
+        ((ACUMACERTOS++))
+        ((ACUMPENALIDADES+=PENALIDADES))
+        ((JAACERTOU = JAACERTOU/60))
+        printf "<td> Yes <small>$TENTATIVAS/$JAACERTOU</small></td>"
+      else
+        PENDINGBLINK=
+        if (( PENDING > 0 )); then
+          PENDINGBLINK="<blink>?Yes?</blink>"
+        fi
+        printf "<td>$PENDINGBLINK<small>$TENTATIVAS/-</small></td>"
+      fi
+    done
+    printf "<td>$ACUMACERTOS ($ACUMPENALIDADES)</td></tr>:$ACUMACERTOS:$ACUMPENALIDADES\n"
+  } > $CONTESTSDIR/$CONTEST/controle/$LOGIN.score
+}
+
+
 mkdir -p $SUBMISSIONDIR-julgados
+mkdir -p $SUBMISSIONDIR-enviaroj
 
 #make $SUBMISSIONDIR world writtable
 chmod 777 $SUBMISSIONDIR
 
-#ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE
+#ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE:$RESP
 for ARQ in $SUBMISSIONDIR/*; do
   if [[ ! -e "$ARQ" ]]; then
     continue
@@ -22,6 +79,7 @@ for ARQ in $SUBMISSIONDIR/*; do
   COMANDO="$(cut -d: -f5 <<< "$N")"
   PROBID="$(cut -d: -f6 <<< "$N")"
   LING="$(cut -d: -f7 <<< "$N")"
+  RESP="$(cut -d: -f8 <<< "$N")"
   #LING="$(file $ARQ|awk '{print $3}')"
 
   #carregar contest
@@ -54,27 +112,14 @@ for ARQ in $SUBMISSIONDIR/*; do
         cut -d: -f1 >> $CONTESTSDIR/$CONTEST/controle/SCORE
     fi
 
-  elif [[ "$COMANDO" == "submit" ]]; then
-    #SITE do problema:
-    SITE=${PROBS[PROBID]}
-
-    #ID no SITE
-    IDSITE=${PROBS[PROBID+1]}
-    login-$SITE
-    CODIGOSUBMISSAO="$(enviar-$SITE "$ARQ" $IDSITE $LING)"
-
-    #aguarda um pouco
-    sleep 3
-
-    RESP="$(pega-resultado-$SITE $CODIGOSUBMISSAO)"
-
-    mkdir -p $CONTESTSDIR/$CONTEST/controle/$LOGIN.d
+  elif [[ "$COMANDO" == "corrigido" ]]; then
 
     PROBIDFILE=$CONTESTSDIR/$CONTEST/controle/$LOGIN.d/$PROBID
 
     PENALIDADES=0
     JAACERTOU=0
     TENTATIVAS=0
+    PENDING=0
     if [[ -e $PROBIDFILE ]]; then
       source $PROBIDFILE
     fi
@@ -85,61 +130,27 @@ for ARQ in $SUBMISSIONDIR/*; do
     if [[ "$RESP" == "Accepted"  && "$JAACERTOU" == "0" ]] ; then
       (( PENALIDADES= PENALIDADES + TEMPO/60 ))
       JAACERTOU=$TEMPO
-      ((TENTATIVAS++))
+      ((PENDING--))
       {
         echo "PENALIDADES=$PENALIDADES"
         echo "JAACERTOU=$JAACERTOU"
         echo "TENTATIVAS=$TENTATIVAS"
+        echo "PENDING=$PENDING"
       } > $PROBIDFILE
-    elif [[ "$JAACERTOU" == "0" ]]; then
-      ((TENTATIVAS++))
+    elif [[ "$JAACERTOU" == "0" && "$RESP" != "Ignored" ]]; then
       ((PENALIDADES+=20))
+      ((PENDING--))
       {
         echo "PENALIDADES=$PENALIDADES"
         echo "JAACERTOU=0"
         echo "TENTATIVAS=$TENTATIVAS"
+        echo "PENDING=$PENDING"
       } > $PROBIDFILE
     fi
 
-    echo "$TEMPO:$LOGIN:$PROBID:$LING:$RESP" >> $CONTESTSDIR/$CONTEST/controle/history
-
-
-    #gerar arquivo para montar o score
-    ACUMPENALIDADES=0
-    ACUMACERTOS=0
-    TAMARRAY=${#PROBS[@]}
-    #Ordem tabela de score
-    #nome | A | B | C | D | ... | Acertos | Penalidade |
-    {
-      NOME="$(grep "^$LOGIN:" $CONTESTSDIR/$CONTEST/passwd|cut -d: -f3)"
-      printf "<td>$NOME</td>"
-      for((prob=0;prob<TAMARRAY;prob+=5)); do
-        PENALIDADES=0
-        JAACERTOU=0
-        TENTATIVAS=0
-        source $CONTESTSDIR/$CONTEST/controle/$LOGIN.d/$prob 2>/dev/null
-        if (( TENTATIVAS == 0 )) ; then
-          printf "<td></td>"
-        elif (( JAACERTOU > 0 )); then
-          ((ACUMACERTOS++))
-          ((ACUMPENALIDADES+=PENALIDADES))
-          ((JAACERTOU = JAACERTOU/60))
-          printf "<td> Yes <small>$TENTATIVAS/$JAACERTOU</small></td>"
-        else
-          #((ACUMPENALIDADES+=PENALIDADES))
-          printf "<td> <small>$TENTATIVAS/-</small></td>"
-        fi
-      done
-      printf "<td>$ACUMACERTOS ($ACUMPENALIDADES)</td></tr>:$ACUMACERTOS:$ACUMPENALIDADES\n"
-    } > $CONTESTSDIR/$CONTEST/controle/$LOGIN.score
-    CLASSIFICACAO=1
-    cat $CONTESTSDIR/$CONTEST/controle/*.score|sort -n -t ':' -k3|
-      sort -s -n -r -t ':' -k2|
-      cut -d: -f1|
-      while read LINE; do
-        echo "<tr><td>$CLASSIFICACAO</td>$LINE";
-        ((CLASSIFICACAO++))
-      done  > $CONTESTSDIR/$CONTEST/controle/SCORE
+    if [[ "$RESP" != "Ignored" ]]; then
+      echo "$TEMPO:$LOGIN:$PROBID:$LING:$RESP" >> $CONTESTSDIR/$CONTEST/controle/history
+    fi
 
 
     #gravar no arquivo de solucoes
@@ -148,6 +159,47 @@ for ARQ in $SUBMISSIONDIR/*; do
     sed -i -e "s/^$ID:.*/$ID:$PROBID:$RESP/" "$USRFILE"
     chmod 777 "$USRFILE"
     cp "$ARQ" $SUBMISSIONDIR-julgados/
+
+    updatedotscore "$LOGIN" "$NOME" "$CONTEST"
+    updatescore $CONTEST
+
+  elif [[ "$COMANDO" == "submit" ]]; then
+    #SITE do problema:
+    SITE=${PROBS[PROBID]}
+
+    PROBIDFILE=$CONTESTSDIR/$CONTEST/controle/$LOGIN.d/$PROBID
+
+    PENALIDADES=0
+    JAACERTOU=0
+    TENTATIVAS=0
+    PENDING=0
+    if [[ -e $PROBIDFILE ]]; then
+      source $PROBIDFILE
+    fi
+
+    if [[ "$JAACERTOU" == "1" ]] ; then
+      RESP=Ignored
+      #gravar no arquivo de solucoes
+      USRFILE="$CONTESTSDIR/$CONTEST/data/$LOGIN"
+      sed -i -e "s/^$ID:.*/$ID:$PROBID:$RESP/" "$USRFILE"
+      chmod 777 "$USRFILE"
+    elif [[ "$JAACERTOU" == "0" ]] ; then
+      cp "$ARQ" $SUBMISSIONDIR-enviaroj/
+
+      ((TENTATIVAS++))
+      ((PENDING++))
+      {
+        echo "PENALIDADES=$PENALIDADES"
+        echo "JAACERTOU=0"
+        echo "TENTATIVAS=$TENTATIVAS"
+        echo "PENDING=$PENDING"
+      } > $PROBIDFILE
+    fi
+
+    NOME="$(grep "^$LOGIN:" $CONTESTSDIR/$CONTEST/passwd|cut -d: -f3)"
+    updatedotscore "$LOGIN" "$NOME" "$CONTEST"
+    updatescore $CONTEST
+
 
     #copiar $ARQ para o diretorio com historico de submissoes
     cp "$ARQ" "$CONTESTSDIR/$CONTEST/submissions/$ID-$LOGIN-${PROBS[PROBID+3]}.$LING"
