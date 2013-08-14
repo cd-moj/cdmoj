0c8173df4d3b42f473635c55e81bef619e5414cc
Bruno Ribas <brunoribas@gmail.com>
2013-08-14 17:19:21 -0300

    enviar-cdmoj.sh: Created script to send and get result from self problems

---
+++ scripts/enviar-cdmoj.sh
@@ -0,0 +1,29 @@
+#ARQFONTE=arquivo-com-a-fonte
+#PROBID=id-do-problema
+
+function link-prob-cdmoj()
+{
+  PROBID=$1
+  echo "nil"
+}
+
+function login-cdmoj()
+{
+  true
+}
+
+#retorna o ID da submissao
+function enviar-cdmoj()
+{
+  ARQFONTE=$1
+  PROBID=$2
+  LINGUAGEM=$3
+  ssh mojjudge@naquadria.brunoribas.com.br "bash autojudge-sh.sh $LINGUAGEM $PROBID" < "$ARQFONTE"
+}
+
+#Retorna string do resultado
+function pega-resultado-cdmoj()
+{
+  JOBID="$1"
+  echo "$JOBID"
+}
