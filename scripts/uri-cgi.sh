36c94838817e39df74761683c2da5c12cdb3174e
Bruno Ribas <brunoribas@gmail.com>
2013-08-27 16:04:31 -0300

    uri-cgi.sh: Added link-prob-uri-pdf
    This function return the closest to a PDF of a problem, it goes to a full
    screen problem page.

--- scripts/uri-cgi.sh
+++ scripts/uri-cgi.sh
@@ -3,3 +3,8 @@ function link-prob-uri()
   local PROBID=$1
   echo "http://www.urionlinejudge.com.br/judge/problems/view/$PROBID"
 }
+function link-prob-uri-pdf()
+{
+  local PROBID=$1
+  echo "http://www.urionlinejudge.com.br/urirepository/UOJ_${PROBID}.html"
+}
