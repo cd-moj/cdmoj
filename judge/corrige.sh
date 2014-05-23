3ef0cff558c80662bcb9239b54a791bd2cafd16c
Bruno Ribas <brunoribas@gmail.com>
2014-05-23 17:56:14 -0300

    corrige.sh: Envia todos códigos e depois que pega os resultados
    Agora o script envia todos os códigos pendentes e somente depois recolhe os
    resultados das submissões.
    
    Dessa maneira a correção pode ser agilizada quando múltiplos sites estão em
    utilização ao mesmo tempo.
    
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- judge/corrige.sh
+++ judge/corrige.sh
@@ -6,7 +6,7 @@ source #SCRIPTSDIR#/enviar-spoj.sh
 source #SCRIPTSDIR#/enviar-uri.sh
 source #SCRIPTSDIR#/enviar-cdmoj.sh
 
-
+PENDING=
 #ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE
 for ARQ in $SUBMISSIONDIR-enviaroj/*; do
   if [[ ! -e "$ARQ" ]]; then
@@ -51,12 +51,39 @@ for ARQ in $SUBMISSIONDIR-enviaroj/*; do
 
   login-$SITE
   CODIGOSUBMISSAO="$(enviar-$SITE "$ARQ" $IDSITE $LING)"
+  PENDING="$PENDING $SITE:$CODIGOSUBMISSAO:$ARQ"
 
-  #aguarda um pouco
-  sleep 3
+done
+
+#dar um tempo para o OJ começar a corrigir
+sleep 3
+echo "-- Pegando Submissões pendentes"
+for SUBMISSIONS in $PENDING; do
+
+  SITE="$(cut -d: -f1 <<< "$SUBMISSIONS")"
+  CODIGOSUBMISSAO="$(cut -d: -f2 <<< "$SUBMISSIONS")"
+  ARQ="$(cut -d: -f3- <<< "$SUBMISSIONS")"
+  N="$(basename "$ARQ")"
+  printf "  $N\n"
+  CONTEST="$(cut -d: -f1 <<< "$N")"
+  ID="$(cut -d: -f2,3 <<< "$N")"
+  LOGIN="$(cut -d: -f4 <<< "$N")"
+  COMANDO="$(cut -d: -f5 <<< "$N")"
+  PROBID="$(cut -d: -f6 <<< "$N")"
+  LING="$(cut -d: -f7 <<< "$N")"
+
+  #carregar contest
+  source $CONTESTSDIR/$CONTEST/conf
+
+  #SITE do problema:
+  SITE=${PROBS[PROBID]}
+
+  #ID no SITE
+  IDSITE=${PROBS[PROBID+1]}
 
   RESP="$(pega-resultado-$SITE "$CODIGOSUBMISSAO")"
 
+  #Se RESP voltar vazio ou ??, significa que deve ser reenviado
   if [[ "${RESP// }" == "" || "${RESP// }" == "??" ]]; then
     continue
   fi
@@ -69,5 +96,4 @@ for ARQ in $SUBMISSIONDIR-enviaroj/*; do
   touch "$SUBMISSIONDIR/$CONTEST:$ID:$LOGIN:$CMDRESP:$PROBID:$LING:$RESP"
 
   rm -f "$ARQ"
-
 done
