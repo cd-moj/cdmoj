26e0b1ca66a230f8443cd76ddd9223058d5de6a4
Bruno Ribas <brunoribas@gmail.com>
2014-05-24 00:09:33 -0300

    bin/cdmoj-delegation-servers.sh: Diretórios possuem cdmoj2 no nome
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- bin/cdmoj-delegation-servers.sh
+++ bin/cdmoj-delegation-servers.sh
@@ -9,25 +9,25 @@ fi
 
 TOTALSERVERS=$1
 
-DIRS=$(ls -d $SUBMISSIONDIR/../cdmoj-delegation-server*|wc -l)
+DIRS=$(ls -d $SUBMISSIONDIR/../cdmoj2-delegation-server*|wc -l)
 
 if (( DIRS == 0 )); then
   for((i=0;i< TOTALSERVERS;i++)); do
-    mkdir -p $SUBMISSIONDIR/../cdmoj-delegation-server$i
+    mkdir -p $SUBMISSIONDIR/../cdmoj2-delegation-server$i
   done
 
 elif (( DIRS > TOTALSERVERS )); then
   for((i=TOTALSERVERS; i< DIRS; i++)); do
-    mv $SUBMISSIONDIR/../cdmoj-delegation-server$i/* $SUBMISSIONDIR/../cdmoj-delegation-server0/
-    rm -rf SUBMISSIONDIR/../cdmoj-delegation-server$i
+    mv $SUBMISSIONDIR/../cdmoj2-delegation-server$i/* $SUBMISSIONDIR/../cdmoj2-delegation-server0/
+    rm -rf SUBMISSIONDIR/../cdmoj2-delegation-server$i
   done
 
 #para desabilitar os delegation servers as submissões pendentes devem ser
 #remanejadas
 elif (( TOTALSERVERS == 0 )); then
   for((i=0; i< DIRS; i++)); do
-    mv $SUBMISSIONDIR/../cdmoj-delegation-server$i/* $SUBMISSIONDIR-enviaroj/
-    rm -rf SUBMISSIONDIR/../cdmoj-delegation-server$i
+    mv $SUBMISSIONDIR/../cdmoj2-delegation-server$i/* $SUBMISSIONDIR-enviaroj/
+    rm -rf SUBMISSIONDIR/../cdmoj2-delegation-server$i
   done
 
 fi
