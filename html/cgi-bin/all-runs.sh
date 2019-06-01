ec3c01beaa72468390ba8bfaea908aa3b708f4c5
Bruno Cesar Ribas <brunoribas@gmail.com>
2019-06-01 01:24:27 -0300

    cgi-bin/all-runs.sh: Optimization
    It is now 3x faster to load
    
    Signed-off-by: Bruno Cesar Ribas <brunoribas@gmail.com>

--- html/cgi-bin/all-runs.sh
+++ html/cgi-bin/all-runs.sh
@@ -15,8 +15,6 @@
 #along with CD-MOJ.  If not, see <http://www.gnu.org/licenses/>.
 
 source common.sh
-AGORA=$(date +%s)
-
 
 #limpar caminho, exemplo
 #www.brunoribas.com.br/~ribas/moj/cgi-bin/contest.sh/contest-teste/oi
@@ -46,39 +44,54 @@ else
 fi
 printf "<h1>Todas Submissões em \"<em>$CONTEST_NAME</em>\"</h1>\n"
 
+declare -A MAPA LINHAMAPA MAPACOMSOLUCAO
+
+declare -A MAPAUSUARIO
+while read l; do
+	MAPAUSUARIO[${l%%:*}]="$(cut -d: -f3 <<< "$l")"
+done < $CONTESTSDIR/$CONTEST/passwd
+
+while read LINE; do
+  #2367:brenno.silva037:0:C:Accepted:1559351367:67ec1806c49888aab55bacf2cb538550
+  readarray -t -d: VET <<< "$LINE"
+  #CODIGO=$(cut -d: -f1,2 <<< $LINE)
+  CODIGO=${VET[5]}:${VET[6]}
+  #TEMPO=$(cut -d: -f1 <<< $LINE)
+  TEMPO=${VET[5]}
+  HORA="$(date --date=@$TEMPO)"
+  #RESP=$(cut -d: -f4 <<< $LINE)
+  RESP="${VET[4]}"
+  #EXERCICIO=$(cut -d: -f3 <<< $LINE)
+  EXERCICIO="${VET[2]}"
+  BGCOLOR=
+  ((LINHAMAPA[${VET[1]}]++))
+  if (( LINHAMAPA[${VET[1]}]%2 == 0 )); then
+    BGCOLOR="bgcolor='#00EEEE'"
+  fi
+  USUARIO=$i
+  ((TEMPO= (TEMPO - CONTEST_START) ))
+  TYPE=${VET[3],}
+  #TYPE="$(echo $TYPE | tr '[A-Z]' '[a-z]')"
+
+  MAPA[${VET[1]}]+="
+  <tr $BGCOLOR><td>${PROBS[$((EXERCICIO+3))]}</td>
+  <td><a target=_blank href='$BASEURL/cgi-bin/getcode.sh/$CONTEST/$CODIGO-$i-${PROBS[$((EXERCICIO+3))]}.$TYPE'>(fonte)</a> $RESP</td>
+  <td>$CODIGO</td><td>$HORA</td>
+  </tr>"
+  MAPACOMSOLUCAO[${VET[1]}]=1
+done < $CONTESTSDIR/$CONTEST/controle/history
+
 cd $CONTESTSDIR/$CONTEST/data/
-for i in *; do
-  NOME="$(grep "^$i:" ../passwd |cut -d: -f3)"
+for i in ${!MAPACOMSOLUCAO[@]}; do
+  NOME="${MAPAUSUARIO[$i]}"
   cat << EOF
   <h2>$NOME ($i)</h2>
   <table border=1>
-  <tr><th>Problema</th><th>Resposta</th><th>Horário da
-  Submissão</th><th>fonte</th></tr>
+  <tr><th>Problema</th><th>Resposta</th><th>Código</th><th>Horário da
+  Submissão</th></tr>
+  ${MAPA[$i]}
+  </table><br/>
 EOF
-  LINHA=1
-  while read LINE; do
-    CODIGO=$(cut -d: -f1,2 <<< $LINE)
-    TEMPO=$(cut -d: -f1 <<< $LINE)
-    HORA="$(date --date=@$TEMPO)"
-    RESP=$(cut -d: -f4 <<< $LINE)
-    EXERCICIO=$(cut -d: -f3 <<< $LINE)
-    BGCOLOR=
-    if (( LINHA%2 == 0 )); then
-      BGCOLOR="bgcolor='#00EEEE'"
-    fi
-    USUARIO=$i
-    ((TEMPO= (TEMPO - CONTEST_START) ))
-    TYPE=$(grep "^$TEMPO:$USUARIO:" $CONTESTSDIR/$CONTEST/controle/history|cut -d: -f4)
-    TYPE="$(echo $TYPE | tr '[A-Z]' '[a-z]')"
-
-    cat <<EOF
-    <tr $BGCOLOR><td>${PROBS[$((EXERCICIO+3))]}</td><td>$RESP</td><td>$HORA</td>
-    <td><a target=_blank href='$BASEURL/cgi-bin/getcode.sh/$CONTEST/$CODIGO-$i-${PROBS[$((EXERCICIO+3))]}.$TYPE'>código</a>
-    </td></tr>
-EOF
-  ((LINHA++))
-  done < $i
-  echo "</table><br/>"
 done
 
 incontest-footer
