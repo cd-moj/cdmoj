034a7a28b00d83aa4854ef8bb390dc971c12b568
Luciano Santos <luciano_z7@hotmail.com>
2022-07-16 19:52:43 -0300

    Adding default user screen

--- html/cgi-bin/clarification.sh
+++ html/cgi-bin/clarification.sh
@@ -15,12 +15,106 @@
 #along with CD-MOJ.  If not, see <http://www.gnu.org/licenses/>.
 
 source common.sh
+AGORA=$(date +%s)
 
-mkdir -p $CACHEDIR/webCat && printf "HTTP/1.1 200 OK\n\n<!doctype html><h2>Erez's netcat chat server!!</h2><form>Username:<br><input type=\"text\" name=\"username\"><br>Message:<br><input type=\"text\" name=\"message\"><div><button>Send data</button></div><button http-equiv=\"refresh\" content=\"0; url=$BASEURL:80\">Refresh</button></form>" > $CACHEDIR/webCat/webpage
-while [ 1 ]
-do
-    [[ $(head -1 $CACHEDIR/webCat/r) =~ "GET /?username" ]] && USER=$(head -1 $CACHEDIR/webCat/r | sed 's@.*username=@@' | sed 's@&message.*@@') && MSG=$(head -1 $CACHEDIR/webCat/r | sed 's@.*message=@@' | sed 's@HTTP.*@@')
-    [ ${#USER} -gt 1 ] && [ ${#MSG} -gt 1 ] && [ ${#USER} -lt 30 ] && [ ${#MSG} -lt 280 ] && printf "\n%s\t%s\n" "$USER" "$MSG" && printf "<h1>%s\t%s" "$USER" "$MSG" >> $CACHEDIR/webCat/webpage
-    cat $CACHEDIR/webCat/webpage | timeout 1 nc -l 1234 > $CACHEDIR/webCat/r
-    unset USER && unset MSG
-done
+POST="$(cat )"
+echo "$POST" > $CACHEDIR/POSTT
+CAMINHO="$PATH_INFO"
+
+#contest é a base do caminho
+CONTEST="$(cut -d'/' -f2 <<< "$CAMINHO")"
+CONTEST="${CONTEST// }"
+
+source $CONTESTSDIR/$CONTEST/conf
+incontest-cabecalho-html $CONTEST
+
+echo "$REQUEST_METHOD"
+if is-admin | grep -q Nao; then
+    TOTPROBS=${#PROBS[@]}
+    for ((i=0;i<TOTPROBS;i+=5)); do
+        SELETOR="$SELETOR <option value=\"$i\">${PROBS[$((i+3))]}</option>"
+    done
+
+    if [ "$(ls -A $CACHEDIR/MSGS)" ]; then
+    cat << EOF
+    <table border="1" width="10%"> <tr><th>Problema</th><th>Resposta</th></tr>
+EOF
+    #for para completar toda a tabela de duvidas ja feitas
+    for ARQ in $CACHEDIR/MSGS/*; do
+	if [[ ! -e "$ARQ" ]]; then
+		continue
+	fi
+	N="$(basename $ARQ)"
+	PROBLEM="$(cut -d: -f1 "$ARQ")"
+	PROBSHORTNAME=${PROBS[$((PROBLEM+3))]}
+  	PROBFULLNAME="${PROBS[$((PROBLEM+2))]}"
+	PROBLEM="$PROBSHORTNAME - $PROBFULLNAME"
+	MSG="$(cut -d: -f2 "$ARQ")"
+	echo "<tr><td>$PROBLEM</td><td>$MSG</td></tr>"	
+	#echo "$MSG"
+    done
+	
+    echo "</table>"
+
+    fi
+
+    if [ "$REQUEST_METHOD" == "GET" ]; then 
+    cat << EOF
+    <body>
+    <form accept-charset="utf-8" enctype="multipart/form-data" action="$BASEURL/cgi-bin/clarification-2.sh/$CONTEST" method="post">
+        <div>
+            <label>Problema: <select name="problems">$SELETOR</select></label><br>
+        </div>
+        <div>
+            <label>Clarification: </label>
+            <textarea name="msg_clarification" rows="4" cols="50"></textarea>
+        </div>
+        <div>
+            <label>Answer: </label>
+            <textarea name="msg_answer" rows="4" cols="50" disabled></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+            <button type="submit" http-equiv="\"refresh\" content=\"0; url=$BASEURL\" formmethod="post">refresh</button>
+        </div>
+    </form>
+    </body>
+EOF
+    elif [ "$REQUEST_METHOD" == "POST" ]; then
+    MSG_CLARIFICATION="$(grep -A2 'name="msg_clarification"' <<< "$POST" |tail -n1|tr -d '\n'|tr -d '\r')"
+    PROBLEM="$(grep -A2 'name="problems"' <<< "$POST" |tail -n1|tr -d '\n'|tr -d '\r')"
+    SHORTPROBLEM="${PROBS[$(($PROBLEM + 3))]}"
+    ((FALTA= (CONTEST_START - AGORA)))
+    ((TIME= (CONTEST_START - FALTA)))
+    TIMEE="$(date --date=@$TIME)"
+    ((TEMPODEPROVA= (TIME - CONTEST_START)/60 ))
+    #Corrigir questao dos acentos
+    MSG="$(echo $MSG_CLARIFICATION | iconv -f UTF8 -t 'ASCII//TRANSLIT')"
+    #echo "$MSG"
+    #mkdir MSGS
+    #Guardando dúvidas para recuperar posteriormente; Tentar identificar usuário pelo login
+    #ORDEMDOPROBLEMA:MENSAGEM:TEMPODEENVIODAMSG:TEMPORESTANTEPROVA
+    echo "$PROBLEM:$MSG_CLARIFICATION:$TIME:$FALTA" > $CACHEDIR/MSGS/$CONTEST:$SHORTPROBLEM
+    cat << EOF
+    <form enctype="multipart/form-data" action="$BASEURL/cgi-bin/clarification.sh/$CONTEST" method="post">
+        <div>
+            <label>Problema: <select name="problems">$SELETOR</select></label><br>
+        </div>
+        <div>
+            <label>Clarification: </label>
+            <textarea name="msg_clarification" rows="4" cols="50" value="$MSG_CLARIFICATION" disabled></textarea>
+        </div>
+        <div>
+            <label>Answer: </label>
+            <textarea name="msg_answer" rows="4" cols="50" disabled></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+            <button type="submit" http-equiv="\"refresh\" content=\"0; url=$BASEURL\" formmethod="post">refresh</button>
+        </div>
+    </form>
+EOF
+    fi
+else
+    echo "ADMIIIIIIIIIIN"
+fi
