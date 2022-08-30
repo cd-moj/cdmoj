2adb73b5610049f68c25d5875a8ca6f323d75f64
Luciano Santos <luciano_z7@hotmail.com>
2022-08-29 22:46:59 -0300

    Removing duplicate code

--- html/cgi-bin/jplag.sh
+++ html/cgi-bin/jplag.sh
@@ -14,6 +14,35 @@
 #You should have received a copy of the GNU General Public License
 #along with CD-MOJ.  If not, see <http://www.gnu.org/licenses/>.
 
+function print-problems(){
+    local TOTPROBS=$1
+    if [[ -f $HTMLDIR/jplag/$CONTEST/matches_avg.csv ]]; then 
+        for ((i=0;i<TOTPROBS;i+=5)); do
+            printf "<h3>${PROBS[$((i+3))]} - ${PROBS[$((i+2))]}</h3>"
+            cat << EOF
+                <table border="1"><tr><th>Submiss&atilde;o 1</th><th>Submiss&atilde;o 2</th><th>Taxa de Pl&aacute;gio</th></tr>
+EOF
+            while read LINE; do
+                PROBLEM="$(cut -d '-' -f3 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
+                SUBMISSION1="$(cut -d '-' -f2 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
+                SUBMISSION2="$(cut -d '-' -f4 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
+                TAXA="$(cut -d ';' -f4 <<< "$LINE" | cut -d ';' -f1)"
+        
+                if [[ "${PROBS[$((i+3))]}" == "$PROBLEM" ]]; then
+                    cat << EOF
+                        <tr>
+                            <td>$SUBMISSION1</td>
+                            <td>$SUBMISSION2</td>
+                            <td><a href="$BASEURL/jplag/$CONTEST/index.html">$TAXA</a></td>
+                        </tr>
+EOF
+                fi
+            done < $HTMLDIR/jplag/$CONTEST/matches_avg.csv 
+            echo "</table><br><br>"
+        done 
+    fi
+}
+
 source common.sh
 
 POST="$(cat )"
@@ -74,59 +103,9 @@ if [[ "$REQUEST_METHOD" == "POST" ]];then
     LINGUAGEM=${LINGUAGENS[$LINGUAGEM]}
     touch  $SUBMISSIONDIR/$CONTEST:$AGORA:$RAND:$LOGIN:jplag:analisar:"$LINGUAGEM"
 
-    if [[ -f $HTMLDIR/jplag/$CONTEST/matches_avg.csv ]]; then 
-        for ((i=0;i<TOTPROBS;i+=5)); do
-            printf "<h3>${PROBS[$((i+3))]} - ${PROBS[$((i+2))]}</h3>"
-            cat << EOF
-                <table border="1"><tr><th>Submiss&atilde;o 1</th><th>Submiss&atilde;o 2</th><th>Taxa de Pl&aacute;gio</th></tr>
-EOF
-            while read LINE; do
-                PROBLEM="$(cut -d '-' -f3 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-                SUBMISSION1="$(cut -d '-' -f2 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-                SUBMISSION2="$(cut -d '-' -f4 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-                TAXA="$(cut -d ';' -f4 <<< "$LINE" | cut -d ';' -f1)"
-        
-                if [[ "${PROBS[$((i+3))]}" == "$PROBLEM" ]]; then
-                    cat << EOF
-                        <tr>
-                            <td>$SUBMISSION1</td>
-                            <td>$SUBMISSION2</td>
-                            <td><a href="$BASEURL/jplag/$CONTEST/index.html">$TAXA</a></td>
-                        </tr>
-EOF
-                fi
-            done < $HTMLDIR/jplag/$CONTEST/matches_avg.csv 
-            echo "</table><br><br>"
-        done 
-    fi
+    print-problems $TOTPROBS
 else
-     if [[ -f $HTMLDIR/jplag/$CONTEST/matches_avg.csv ]]; then
-            for ((i=0;i<TOTPROBS;i+=5)); do
-                printf "<h2>${PROBS[$((i+3))]} - ${PROBS[$((i+2))]}</h2>"
-                cat << EOF
-	       	        <table border="1"><tr><th>Submiss&atilde;o 1</th><th>Submiss&atilde;o 2</th><th>Taxa de Pl&aacute;gio</th></tr>
-EOF
-                while read LINE; do
-                    PROBLEM="$(cut -d '-' -f3 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-                    SUBMISSION1="$(cut -d '-' -f2 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-                    SUBMISSION2="$(cut -d '-' -f4 <<< "$LINE" | cut -d. -f1 | cut -d ';' -f1)"
-		            TAXA="$(cut -d ';' -f4 <<< "$LINE" | cut -d ';' -f1)"
-		    
-                    if [[ "${PROBS[$((i+3))]}" == "$PROBLEM" ]]; then
-                        cat << EOF
-                                <tr>
-                                    <td>$SUBMISSION1</td>
-                                    <td>$SUBMISSION2</td>
-                                    <td><a href="$BASEURL/jplag/$CONTEST/index.html">$TAXA</a></td>
-                                </tr>
-EOF
-                    fi
-                done < $HTMLDIR/jplag/$CONTEST/matches_avg.csv 
-		echo "</table><br><br>"
-            done
-        
-    fi
-    
+    print-problems $TOTPROBS
 fi
 
 incontest-footer
