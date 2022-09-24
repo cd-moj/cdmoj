fdda7b8611c263c764f56da795c97d6c3830a347
Bruno Ribas <brunoribas@gmail.com>
2022-09-24 19:31:56 -0300

    julgador.sh: Verifica Accepted como parte, por causa da porcentagem e PE novos
    Signed-off-by: Bruno Ribas <brunoribas@gmail.com>

--- judge/julgador.sh
+++ judge/julgador.sh
@@ -253,7 +253,7 @@ for ARQ in $SUBMISSIONDIR/*; do
     TEMPO="$(cut -d: -f1 <<< "$ID")"
     ((TEMPO= (TEMPO - CONTEST_START) ))
 
-    if [[ "$RESP" == "Accepted"  && "$JAACERTOU" == "0" ]] ; then
+    if [[ "$RESP" =~ "Accepted"  && "$JAACERTOU" == "0" ]] ; then
       JAACERTOU=$TEMPO
     elif [[ "$JAACERTOU" != "0" ]] ; then
       RESP="$RESP (Ignored)"
