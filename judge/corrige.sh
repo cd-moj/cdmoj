40ceee1cf9c919fcb91586c1595ccc28557090b2
Bruno Ribas <brunoribas@gmail.com>
2023-06-02 23:11:38 -0300

    j/corrige.sh: grab verdict in parallel reducing total wait

--- judge/corrige.sh
+++ judge/corrige.sh
@@ -14,77 +14,30 @@
 #You should have received a copy of the GNU General Public License
 #along with CD-MOJ.  If not, see <http://www.gnu.org/licenses/>.
 
-source #CONFDIR#/judge.conf
-source #CONFDIR#/common.conf
-source #SCRIPTSDIR#/enviar-spoj.sh
-source #SCRIPTSDIR#/enviar-uri.sh
-source #SCRIPTSDIR#/enviar-cdmoj.sh
-source #SCRIPTSDIR#/enviar-cdmoj2.sh
+source /home/moj/moj/etc/common.conf
+source /home/moj/moj/etc/judge.conf
+source /home/moj/moj/scripts/enviar-spoj.sh
+source /home/moj/moj/scripts/enviar-uri.sh
+source /home/moj/moj/scripts/enviar-cdmoj.sh
+source /home/moj/moj/scripts/enviar-cdmoj2.sh
 
-PENDING=
-LOGGEDIN=
-COUNT=0
-#ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE
-for ARQ in $SUBMISSIONDIR-enviaroj/*submit* $SUBMISSIONDIR-enviaroj/*rejulgar*; do
-  if [[ ! -e "$ARQ" ]]; then
-    continue
-  fi
-  N="$(basename $ARQ)"
-  printf "\n$N\n"
-  CONTEST="$(cut -d: -f1 <<< "$N")"
-  ID="$(cut -d: -f2,3 <<< "$N")"
-  LOGIN="$(cut -d: -f4 <<< "$N")"
-  COMANDO="$(cut -d: -f5 <<< "$N")"
-  PROBID="$(cut -d: -f6 <<< "$N")"
-  LING="$(cut -d: -f7 <<< "$N")"
-
-  if [[ "x$LING" == "x" ]]; then
-    LING="UNKNOWN"
-  fi
-
-  #carregar contest
-  source $CONTESTSDIR/$CONTEST/conf
-
-  #SITE do problema:
-  SITE=${PROBS[PROBID]}
-
-  #ID no SITE
-  IDSITE=${PROBS[PROBID+1]}
-
-  if ! grep -q "\<$SITE\>" <<< "$LOGGEDIN" ; then
-    login-$SITE
-    LOGGEDIN="$LOGGEDIN $SITE"
-  fi
-
-  if [[ "$LANGUAGES" == "" ]] || grep -q "$LING" <<< "$LANGUAGES"; then
-    CODIGOSUBMISSAO="$(enviar-$SITE "$ARQ" $IDSITE $LING|tr ' ' '_')"
-    unset LANGUAGES
-  else
-    CODIGOSUBMISSAO="Wrong_Language_Choice"
-  fi
-
-  PENDING="$PENDING $SITE:$CODIGOSUBMISSAO:$ARQ"
-  ((COUNT++))
-  (( COUNT >= 50 )) && break
-
-done
-
-#dar um tempo para o OJ começar a corrigir
-sleep 1
-echo "-- Pegando Submissões pendentes"
-for SUBMISSIONS in $PENDING; do
-
-  SITE="$(cut -d: -f1 <<< "$SUBMISSIONS")"
-  CODIGOSUBMISSAO="$(cut -d: -f2 <<< "$SUBMISSIONS"|tr '_' ' ')"
-  ARQ="$(cut -d: -f3- <<< "$SUBMISSIONS")"
-  N="$(basename "$ARQ")"
-  printf "  $N\n"
-  CONTEST="$(cut -d: -f1 <<< "$N")"
-  ID="$(cut -d: -f2,3 <<< "$N")"
-  LOGIN="$(cut -d: -f4 <<< "$N")"
-  COMANDO="$(cut -d: -f5 <<< "$N")"
-  PROBID="$(cut -d: -f6 <<< "$N")"
-  LING="$(cut -d: -f7 <<< "$N")"
+ANIMACAO='/-\|'
+ANIPOS=0
+function pegaresultado()
+{
+  local SUBMISSIONS="$1"
+  local SITE="$(cut -d: -f1 <<< "$SUBMISSIONS")"
+  local CODIGOSUBMISSAO="$(cut -d: -f2 <<< "$SUBMISSIONS"|tr '_' ' ')"
+  local ARQ="$(cut -d: -f3- <<< "$SUBMISSIONS")"
+  local N="$(basename "$ARQ")"
+  echo "--  $N"
+  local CONTEST="$(cut -d: -f1 <<< "$N")"
+  local ID="$(cut -d: -f2,3 <<< "$N")"
+  local LOGIN="$(cut -d: -f4 <<< "$N")"
+  local COMANDO="$(cut -d: -f5 <<< "$N")"
+  local PROBID="$(cut -d: -f6 <<< "$N")"
+  local LING="$(cut -d: -f7 <<< "$N")"
+  local RESP
 
   #carregar contest
   source $CONTESTSDIR/$CONTEST/conf
@@ -103,7 +56,7 @@ for SUBMISSIONS in $PENDING; do
 
   #Se RESP voltar vazio ou ??, significa que deve ser reenviado
   if [[ "${RESP// }" == "" || "${RESP// }" == "??" ]]; then
-    continue
+    return
   fi
 
   #ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE
@@ -114,4 +67,86 @@ for SUBMISSIONS in $PENDING; do
   touch "$SUBMISSIONDIR/$CONTEST:$ID:$LOGIN:$CMDRESP:$PROBID:$LING:$RESP"
 
   rm -f "$ARQ"
+
+}
+
+declare -A PENDING PENDINGPID
+declare -A LOGGEDIN
+COUNT=0
+#ordem de ARQ: $CONTEST:$AGORA:$RAND:$LOGIN:comando:$PROBLEMA:$FILETYPE
+
+cd $SUBMISSIONDIR-enviaroj
+X=0
+while true; do
+	if (( $(ls |wc -l) == 0 )); then
+		(( X % 6 == 0 )) && printf "."
+		sleep 0.5
+		((X++))
+		continue
+	fi
+	X=0
+  for ARQ in $SUBMISSIONDIR-enviaroj/*submit* $SUBMISSIONDIR-enviaroj/*rejulgar*; do
+    if [[ ! -e "$ARQ" ]]; then
+      continue
+    fi
+    [[ -n "${PENDING[$ARQ]}" ]] && continue
+    N="$(basename $ARQ)"
+    printf "\n$N\n"
+    CONTEST="$(cut -d: -f1 <<< "$N")"
+    ID="$(cut -d: -f2,3 <<< "$N")"
+    LOGIN="$(cut -d: -f4 <<< "$N")"
+    COMANDO="$(cut -d: -f5 <<< "$N")"
+    PROBID="$(cut -d: -f6 <<< "$N")"
+    LING="$(cut -d: -f7 <<< "$N")"
+
+    if [[ "x$LING" == "x" ]]; then
+      LING="UNKNOWN"
+    fi
+
+    #carregar contest
+    source $CONTESTSDIR/$CONTEST/conf
+
+    #SITE do problema:
+    SITE=${PROBS[PROBID]}
+
+    #ID no SITE
+    IDSITE=${PROBS[PROBID+1]}
+
+    if [[ -z "${LOGGEDIN[$SITE]}" ]] || (( EPOCHSECONDS - ${LOGGEDIN[$SITE]} > 300 )); then
+      login-$SITE
+      LOGGEDIN[$SITE]=$EPOCHSECONDS
+    fi
+
+    if [[ "$LANGUAGES" == "" ]] || grep -q "$LING" <<< "$LANGUAGES"; then
+      CODIGOSUBMISSAO="$(enviar-$SITE "$ARQ" $IDSITE $LING|tr ' ' '_')"
+      unset LANGUAGES
+    else
+      CODIGOSUBMISSAO="Wrong_Language_Choice"
+    fi
+
+    PENDING[$ARQ]="$SITE:$CODIGOSUBMISSAO:$ARQ"
+
+  done
+  #dar um tempo para o OJ começar a corrigir
+  sleep 1
+  #echo -e "\n-- Pegando Submissões pendentes"
+  echo -en "${ANIMACAO:$ANIPOS:1} \r"
+  ((ANIPOS=(ANIPOS+1)%${#ANIMACAO}))
+  for ARQ in ${!PENDING[@]}; do
+    #[[ ! -e "$ARQ" ]] && unset PENDING[$ARQ] && unset PENDINGPID[$ARQ,PID] && echo "  -- ja pegou" && continue
+    if [[ -n ${PENDINGPID[$ARQ,PID]} ]]; then
+      [[ -d "/proc/${PENDINGPID[$ARQ,PID]}" ]] && continue
+      echo -e "\n-- $(basename $ARQ)"
+      echo -n "  -- verificando PID(${PENDINGPID[$ARQ,PID]})"
+      [[ ! -e "$ARQ" ]] && echo ".. CONCLUIU após $((EPOCHSECONDS-PENDINGPID[$ARQ,TIME])) segundos"
+      [[ -e "$ARQ" ]] && echo ".. DESISTIU após $((EPOCHSECONDS-PENDINGPID[$ARQ,TIME])) segundos. Recolocando na FILA..."
+      unset PENDING[$ARQ] PENDINGPID[$ARQ,PID]
+      continue
+    fi
+    pegaresultado "${PENDING[$ARQ]}" &
+    PENDINGPID[$ARQ,PID]=$!
+    PENDINGPID[$ARQ,TIME]=$EPOCHSECONDS
+  done
 done
+
+wait
