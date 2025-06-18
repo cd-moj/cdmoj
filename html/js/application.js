9851f4adaf05f6881179c424b93527a7996ea917
Bruno Ribas <ribas@naquadah.com.br>
2025-06-17 23:52:20 -0300

    Various html/ files synced with version running at moj.naquadah.com.br
    Signed-off-by: Bruno Ribas <ribas@naquadah.com.br>

--- html/js/application.js
+++ html/js/application.js
@@ -2,15 +2,15 @@ function cufon() {
     Cufon.replace('#etapas', {
         fontFamily: "AvantGarde Bk BT"
     });
-    Cufon.replace('#footer', {
+/*    Cufon.replace('#footer', {
         fontFamily: "AvantGarde Bk BT"
-    });
+    }); */
     Cufon.replace('#menu', {
         fontFamily: "AvantGarde Bk BT"
     });
-    Cufon.replace('h1', {
+/*    Cufon.replace('h1', {
         fontFamily: "AvantGarde Bk BT"
-    });
+    }); */
     Cufon.replace('#menu1', {
         fontFamily: "AvantGarde Bk BT"
     });
